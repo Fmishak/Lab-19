32797b926835fd805a7f2413dee3b90fe9b9487b
Farouk Mishak <farouk.mishak@gmail.com>
2019-05-18 02:03:58 -0700

    Initial Commit

---
+++ Lab #19/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab #19
+//
+//  Created by Farouk Mishak on 5/18/19.
+//  Copyright © 2019 Farouk Mishak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
